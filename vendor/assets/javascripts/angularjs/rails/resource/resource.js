77de64b70cd9796c29530b72d43cf3d4a133cd5a
Valentine Zavadsky <saicheg@gmail.com>
2014-08-27 16:00:09 +0300

    add ability to abort request

--- vendor/assets/javascripts/angularjs/rails/resource/resource.js
+++ vendor/assets/javascripts/angularjs/rails/resource/resource.js
@@ -541,6 +541,8 @@
                 RailsResource.$http = function (httpConfig, context, resourceConfigOverrides) {
                     var config = angular.extend(angular.copy(this.config), resourceConfigOverrides || {}),
                         resourceConstructor = config.resourceConstructor,
+                        requestCanceler = $q.defer(),
+                        httpConfig = angular.extend({timeout: requestCanceler.promise}, httpConfig),
                         promise = $q.when(httpConfig);
 
                     if (!config.skipRequestProcessing) {
@@ -606,6 +608,9 @@
                     promise = this.runInterceptorPhase('afterResponse', context, promise);
                     promise.resource = config.resourceConstructor;
                     promise.context = context;
+
+                    promise.abort = function() { return requestCanceler.resolve(); }
+
                     return promise;
                 };
 
